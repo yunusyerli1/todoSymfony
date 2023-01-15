23b0b5100dc19ec6288cb603411054c36543c8ae
yunusyerli1 <yunusyerli1@gmail.com>
2023-01-15 16:55:52 +0300

    initial db set

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+    private const POSTS = [
+      [
+          'id' => 1,
+          'slug' => 'hello-world',
+          'title'=> 'Hello World'
+      ],
+        [
+            'id' => 2,
+            'slug' => 'another-post',
+            'title'=> 'This is another post'
+        ],
+        [
+            'id' => 3,
+            'slug' => 'last-example',
+            'title'=> 'This is the last example'
+        ],
+
+    ];
+
+
+
+
+    /**
+     * @Route("/", name="blog_list", defaults={"page":5})
+     */
+    public function list($page)
+    {
+        return new JsonResponse( [
+            'page' => $page,
+            'data'=> self::POSTS
+        ]);
+    }
+
+    /**
+     * @Route("/routes", name="blog_routes", defaults={"page":5})
+     */
+    public function listRoute($page=2, Request $request)
+    {
+        $limit= $request->get('limit', 10);
+        return $this->json([
+            'page' => $page,
+            'limit'=> $limit,
+            'data'=> array_map(function ($item){
+                return $this->generateUrl('blog_by_slug', ['slug'=> $item['slug']]);
+            },self::POSTS)
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     */
+    public function post($id)
+    {
+        return new JsonResponse(
+          self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+        );
+    }
+
+    /**
+     * @Route("/{slug}", name="blog_by_slug")
+     */
+    public function postBySlug($slug)
+    {
+        return new JsonResponse(
+            self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+        );
+    }
+
+
+
+}
---
+++ src/Entity/BlogPost.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlogPostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BlogPostRepository::class)]
+class BlogPost
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $published = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPubs(): ?\DateTimeInterface
+    {
+        return $this->pubs;
+    }
+
+    public function setPubs(\DateTimeInterface $published): self
+    {
+        $this->pubs = $published;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
